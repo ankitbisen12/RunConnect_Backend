a9ac00f5a6a214e4b9c97acc48ff5e004495666d
Ankit Bisen <ankitbisen751@gmail.com>
2026-01-27 00:22:59 +0530

    Add Event and User schemas and implement password hashing in User schema

---
+++ models/eventModel.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Event must have a name"],
+      unique: true,
+      maxLength: [200, "Event name should not exceed 200 characters"],
+      minLength: [10, "Event name should have at least 5 characters"],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"],
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: true,
+    },
+    maxParticipants: {
+      type: Number,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, "Event must have a duration"],
+    },
+    eventType: {
+      type: String,
+      required: [true, "Event must have a type"],
+      enum: {
+        values: ["Running", "Cycling"],
+        message: "Event type is either: online, in-person, hybrid",
+      },
+    },
+    price: {
+      type: Number,
+      required: [true, "Event must have a price"],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE})should be below regular price",
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Event = mongoose.model("Event", eventSchema);
+export default Event;
---
+++ models/userModel.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please tell us your name"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide your email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minLength: [10, "Password must be at least 8 characters long"],
+        select: false,
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, "Please confirm your password"],
+        //This only works on CREATE and SAVE!!!
+        validate: {
+            validator: function (el) {
+                return el === this.password; /// password == passwordConfirm
+            },
+            message: "Passwords are not the same!",
+        },
+    },
+    role: {
+        type: String,
+        enum: ["User", "Admin", "Organizer"],
+        default: "User",
+    },
+    active: {
+        type: Boolean,
+        default: true,
+        select: false    /// hide from output
+    },
+    passwordChangedAt: Date,
+});
+
+//Document middleware.
+userSchema.pre("save", async function (next) {
+    //if Password is not modified then skip hashing.
+    if (!this.isModified("password")) return next();
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //delete confirmPassweord field. Need just for verification.
+    this.confirmPassword = undefined;
+    next();
+});
+
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password") || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000; //to capture password change time.
+    next();
+});
+
+userSchema.pre(/^find/, function () {
+    this.find({ active: { $ne: false } });
+    next();
+});
+
+//Instance method.
+//at the time of login, checking login password and saved password is matching or not.
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
